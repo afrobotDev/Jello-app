3decd7eb90dbfac18a6c8c4398210cdbe8e60a7e
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-03 15:07:59 +0300

    Fetch projects from boot.dev api using random API key and log projects title and completion status

---
+++ URIs/url_paths.ts
@@ -0,0 +1,31 @@
+const url = "https://api.boot.dev/v1/courses_rest_api/learn-http/projects";
+
+import { Project } from "./url_paths_types";
+
+const apiKey = generateKey();
+const response = await fetch(url, {
+  method: "GET",
+  mode: "cors",
+  headers: {
+    "X-API-Key": apiKey,
+    "Content-Type": "application/json",
+  },
+});
+
+const responseData = await response.json();
+
+logProjects(responseData);
+
+function generateKey(): string {
+  const characters = "ABCDEF0123456789";
+  let result = "";
+  for (let i = 0; i < 16; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+function logProjects(projects: Project[]) {
+  for (const project of projects) {
+    console.log(`Project: ${project.title}, Completed: ${project.completed}`);
+  }
+}
