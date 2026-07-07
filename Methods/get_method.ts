0f1eea7e6bc3c387b6fc4d12ed53d2661ecc19f5
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-07 21:28:49 +0300

    Fetch a list of users frome a RESTAPI via GET request, then logs each user's name, role, experience and remote status

---
+++ Methods/get_method.ts
@@ -0,0 +1,35 @@
+import { UserProfile } from "./types";
+
+async function getUsers(url: string, apiKey: string): Promise<UserProfile[]> {
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "X-API-KEY": apiKey,
+      "Content-Type": "application/json",
+    },
+  });
+  return response.json();
+}
+
+const generatedKey = generateKey();
+const url = "https://api.boot.dev/v1/courses_rest_api/learn-http/users";
+const users = await getUsers(url, generatedKey);
+logUsers(users);
+
+function generateKey(): string {
+  const characters = "ABCDEF0123456789";
+  let result = "";
+  for (let i = 0; i < 16; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+function logUsers(users: UserProfile[]) {
+  for (const user of users) {
+    console.log(
+      `User name: ${user.user.name}, Role: ${user.role}, experience: ${user.experience}, Remote: ${user.remote}`,
+    );
+  }
+}
